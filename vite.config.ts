fa3802d8e11cc4e18ae2e8c6c62478a8bdaed985
Yailuang Chaorainakkunsi <khao@khaodoes.dev>
2025-04-24 17:47:28 +0700

    Add PWA support with vite-plugin-pwa and update configurations
    - Integrate vite-plugin-pwa for progressive web app capabilities.
    - Update vite.config.ts to include PWA configuration with app manifest and icons.
    - Add vite-plugin-pwa dependency to package.json and bun.lock for asset management.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,56 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+			registerType: "autoUpdate",
+      includeAssets: ["icon.png"],
+      manifest: {
+        name: "Khao's Timestamp App",
+        short_name: "khaos-timestamp-app",
+        description: "A simple timestamp app by KhaoDoesDev.",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
